8ace180c99e0d39c3e0d12c0b27299d70381a739
rushikeshbhosale185 <rushikeshbhosale185@gmail.com>
2023-04-18 15:56:13 +0530

    first commit

---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import contact from './contact.png'
+
+const ContactUs = () => {
+  return (
+    <div className='main'>
+      <img src={contact} alt="Not Found" />
+      <br />
+      <h2>PrepBytes</h2>
+      <h3>Education center in Gurgaon, Haryana</h3>
+      <h4>Sector 21E, Sector 21, Gurugram, Haryana 122016 <br />Contact. No. :- 8800258817 <br /> Contact Email :- contact@prepbytes.com <br /> Founder :- Mamta Kumari <br /> Founded :- 2008 , No Of Employees :- 1036</h4>
+    </div>
+  )
+}
+
+export default ContactUs
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import web from './web.jpg'
+
+const Home = () => {
+  return (
+    <div>
+      <br />
+      <br />
+      <img src={web} alt="Not Found" className='img' />
+    </div>
+  )
+}
+
+export default Home
